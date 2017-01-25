797e6c2506e5bf3596eb42f4c1b02e2c1e30ee0c
Andrzej Siwek <a.siwek@impact-it.pl>
2017-01-25 13:02:31 +0100

    Add project files.

---
+++ KursPodstawowyRemastered/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KursPodstawowyRemastered
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        // Zmienna
+        public int a = 1;
+
+        // Konstruktor klasy
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        //Metoda
+        public void PierwszaMetoda()
+        {
+
+        }
+    }
+}

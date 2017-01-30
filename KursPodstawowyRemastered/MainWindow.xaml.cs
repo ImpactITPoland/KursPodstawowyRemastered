5ebdd7bfff7c3eda720fc8771891b0f55d3dca0c
Andrzej Siwek <a.siwek@impact-it.pl>
2017-01-30 10:29:55 +0100

    Operatory

--- KursPodstawowyRemastered/MainWindow.xaml.cs
+++ KursPodstawowyRemastered/MainWindow.xaml.cs
@@ -56,6 +56,52 @@ namespace KursPodstawowyRemastered
         int liczbaPolubien = 10; // Testowa zmienna
         float r = 10f; // zmienna pomocnicza
 
+        public void Operatory()
+        {
+            #region Zmienne pomocnicze
+
+            int zmiennaLiczbowa1 = 10;
+            int zmiennaLiczbowa2 = 20;
+
+            bool zmiennaLogiczna1 = true;
+            bool zmiennaLogiczna2 = false;
+
+            #endregion
+
+            // Operatory matematyczne
+            var wynik1 = zmiennaLiczbowa1 + zmiennaLiczbowa2; // opeartor dodawania
+            var wynik2 = zmiennaLiczbowa1 - zmiennaLiczbowa2; // opeartor różnicy (odejmowania)
+            var wynik3 = zmiennaLiczbowa1 * zmiennaLiczbowa2; // opeartor mnożenia
+            var wynik4 = zmiennaLiczbowa1 / zmiennaLiczbowa2; // opeartor dzielenia
+            var wynik5 = zmiennaLiczbowa1 % zmiennaLiczbowa2; // opeartor dzielenia z resztą
+
+            var wynik6 = zmiennaLiczbowa1++; // operator inkrementacji
+            var wynik7 = zmiennaLiczbowa1--; // operator dekrementacji
+
+            // Operatory relacji (najpopularniejsze)
+            var wynik8 = (zmiennaLiczbowa1 == zmiennaLiczbowa2); // operator porównania
+            var wynik9 = (zmiennaLiczbowa1 != zmiennaLiczbowa2); // operator negacji
+            var wynik10 = (zmiennaLiczbowa1 > zmiennaLiczbowa2); // operator A większe od B (większości)
+            var wynik11 = (zmiennaLiczbowa1 >= zmiennaLiczbowa2); // operator A większe lub równe B
+            var wynik12 = (zmiennaLiczbowa1 < zmiennaLiczbowa2); // operator A mniejsze od B (mniejszości)
+            var wynik13 = (zmiennaLiczbowa1 <= zmiennaLiczbowa2); // operator A mniejsze lub równe B
+
+            // Operatory logiczne (najpopularniejsze)
+            var wynik14 = (zmiennaLogiczna1 && zmiennaLogiczna2); // zwróci [true] gdy A i B mają wartośc true (suma logiczna)
+            var wynik15 = (zmiennaLogiczna1 || zmiennaLogiczna2); // zwróci [true] gdy A lub B mają wartośc true (alternatywa logiczna)
+            var wynik16 = (!zmiennaLogiczna1); // zwróci [true] A ma wartośc false (negacja logiczna)
+            var wynik16 = (!zmiennaLogiczna1); // zwróci [true] A ma wartośc false (negacja logiczna)
+
+
+
+            // Operatory matematyczne - sztuczki czyli skracanie zapisu
+            zmiennaLiczbowa1 += 3; // skraca zapis => A = A + 3
+            zmiennaLiczbowa1 =+ 3; // skraca zapis => A = 3 + A
+            zmiennaLiczbowa1 =- 3; // skraca zapis => A = 3 - A
+            zmiennaLiczbowa1 *= 3; // skraca zapis => A = 3 * A
+
+        }
+
         //Metoda
         public void PierwszaMetoda()
         {
